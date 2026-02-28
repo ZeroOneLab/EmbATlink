c8075de41e6b84d20943dafb97bc25005d306793
不要染上嵌入式 <wang2869902214@outlook.com>
2026-02-28 17:26:24 +0800

    update README

--- EmbATlink/Example/EmbATlink/at_driver.c
+++ EmbATlink/Example/EmbATlink/at_driver.c
@@ -1,18 +1,23 @@
-/*
- * Copyright (c) 2026 零壹实验室-ZoreOneLab
- * Repository: https://github.com/ZeroOneLab/EmbATlink.git
- * Tutorial: https://www.yuque.com/u54102073/rs06a5/lgcn1lumuhqfvcgg?singleDoc#
- * 
+/**
+ * @file    at_driver.c
+ * @version v1.0
+ * @date    2026-02-28
+ * @author  ZeroOneLab
+ * @website https://github.com/ZeroOneLab/EmbATlink.git
+ *
+ * @license MIT License
+ * Copyright (c) 2026 ZeroOneLab
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
